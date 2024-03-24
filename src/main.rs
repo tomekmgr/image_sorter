693d8a6325185d073bf1a99961e48fc5af611d0f
mgr <tomekmgr@gmail.com>
2024-03-24 10:21:58 +0100

    Add spinner, improve messages

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,56 @@
-use std::{fs, path::{Path, PathBuf}, str::FromStr};
+use std::{env, fs, path::{Path, PathBuf}};
 use image;
 use chrono::{DateTime, Datelike, Utc};
+use spinners:: {Spinner, Spinners};
 
 fn main() {
     let mut files: Vec<PathBuf> = vec![];
 
-    scanner(PathBuf::from_str(r".").unwrap(), &mut files, false);
-    mover(&files);
+    println!("Rusty image sorter v. 0.5");
+    if let Ok(current_dir) = env::current_dir() {
+        //spinner initialization 
+        let mut spi = Spinner::new(Spinners::Dots, "\tLooking for image files in a current working folder...".into());
 
-    println!("Hello, world! {}", files.len());
+        scanner(current_dir, &mut files, false);
+        spi.stop();
+
+        println!("\nDetected images count: {} file(s)", files.len());
+        if files.len() > 0 {
+            //show spinner one more time
+            let mut spi = Spinner::new(Spinners::Dots, "\tSorting images...".into());
+            mover(&files);
+            spi.stop()
+        }
+    } else {
+        println!("Error getting current directory");
+    }
 }
-
 
 fn mover(files: &Vec<PathBuf>) {
     for file in files.iter() {
         println!("{}", file.to_str().unwrap());
         if let Ok(time) = file.metadata().unwrap().modified() {
             let dd: DateTime<Utc> = time.into();
-            println!("{time:?}  {}", dd);
             let pku = Path::new(file);
             let mut des = PathBuf::new();
             des.push(pku.parent().unwrap());
             des.push(dd.year().to_string());
             des.push(dd.month().to_string());
             des.push(file.file_name().unwrap());
-            println!("{}", des.to_str().unwrap());
             match fs::create_dir_all(des.parent().unwrap()) {
                 Ok(_) => {
                     let res = fs::rename(file, des);
                     match res {
-                        Ok(_) => {println!("File moved")}
+                        Ok(_) => {}
                         Err(e) => {println!("Error moving a file! {}", e)}
                     }
                 }
                 Err(e) => {println!("Error creating folder structure. {}", e)}
             }
-            
         }
-
     }
 
 }
-
 
 
 fn scanner(path: PathBuf, files: &mut Vec<PathBuf>, recursive: bool) {
@@ -75,19 +84,16 @@ fn scanner(path: PathBuf, files: &mut Vec<PathBuf>, recursive: bool) {
                                     //println!("NO extenstion {}", curr_path.file_name().unwrap().to_str().unwrap());
                                 }
                             }
-//                            println!("{}", p.path().extension().unwrap().to_str().unwrap());                       
-                            //files.push(curr_path);
                         }
                     },
                     Err(e) => {
-                        println!("Erorex {}", e);
+                        println!("Error resolving path {}", e);
                     }
                 }
             }
-        },
-
+        }
         Err(e) => {
-            println!("yeah, error! {}", e);
+            println!("Error opening a directory. {}", e);
         }
     }
 }
